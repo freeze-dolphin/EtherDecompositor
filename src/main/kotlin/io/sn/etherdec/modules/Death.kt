22a43bb4f49a6109bae0e3ff9e8896df0102425c
Sapphira Tempest <freeze-dolphin@outlook.com>
2023-08-26 22:20:32 +0800

    feat: 现在玩家死亡掉的钱会把击杀者拿走

--- src/main/kotlin/io/sn/etherdec/modules/Death.kt
+++ src/main/kotlin/io/sn/etherdec/modules/Death.kt
@@ -6,7 +6,10 @@ import io.sn.etherdec.objects.AListener
 import io.sn.etherdec.objects.AbstractModule
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import kotlin.math.min
 import kotlin.random.Random
@@ -26,14 +29,32 @@ class Death(plug: EtherCore) : AbstractModule(plug), AListener {
             )
         }
 
-        if (evt.player.world.name == "world") return
+        evt.keepLevel = true
+    }
 
-        val rnd = Random.nextInt(10, 32).toDouble()
-        val acc = Balance.balance(evt.player)
-        val droped = min(rnd, acc)
-        if (droped < 1) return
-        Balance.remove(evt.player, droped)
-        evt.player.sendMessage(EtherCore.minid("<dark_gray>[<red>喜报<dark_gray>] <white>你因为死亡丢失了 <green>${droped} <dark_green>E"))
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onKilled(evt: EntityDamageByEntityEvent) {
+        if (evt.entity is Player) {
+            val player = evt.entity as Player
+            if (evt.finalDamage >= player.health) {
+
+                if (player.world.name == "world") return
+
+                val rnd = Random.nextInt(10, 32).toDouble()
+
+                val acc = Balance.balance(player)
+                val droped = min(rnd, acc)
+                if (droped < 1) return
+                Balance.remove(player, droped)
+                player.sendMessage(EtherCore.minid("<dark_gray>[<red>喜报<dark_gray>] <white>你因为死亡丢失了 <green>${droped} <dark_green>E"))
+
+                if (evt.damager is Player) {
+                    val damager = evt.damager as Player
+                    damager.sendMessage(EtherCore.minid("<dark_gray>[<green>经济<dark_gray>] <white>你因为击杀 <yellow>${player.name} <white>获得了 <green>${droped} <dark_green>E"))
+                    Balance.add(damager, droped)
+                }
+            }
+        }
     }
 
 }
